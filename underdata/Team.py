06d0ae7d1e362afa3d0dd0ef98a376e277d2638f
Osvaldo Trujillo <osvaldotrujillo89@gmail.com>
2021-10-28 02:18:00 -0500

    [ADD] Team crawler

---
+++ underdata/Team.py
@@ -0,0 +1,15 @@
+"""
+UnderData v0.1
+"""
+import pandas as pd
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+from . import LEAGUES
+
+class Team():
+    """
+    """
+    def __init__(self):
+        pass
