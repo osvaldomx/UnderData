71d20f46604d483820e73d078e85246b29b60f2c
Osvaldo Trujillo <osvaldo.trujillo.ingenieria@gmail.com>
2025-08-14 01:22:31 -0600

    [UPDATE] Minor changes.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as file:
 
 setuptools.setup(
     name="underdata",
-    version="0.1.2",
+    version="0.2.0",
     author="Osvaldo Trujillo",
     author_email="osvaldo.trujillo.ingenieria@gmail.com",
     description="Scraping data package for www.understat.com",
@@ -14,13 +14,13 @@ setuptools.setup(
     url="https://github.com/osvaldomx/UnderData",
     packages=setuptools.find_packages(),
     license="MIT",
-    install_requires=["requests>=2.26.0", "selenium>=4.0.0", "pandas>=1.3.4"],
-    python_requires = ">=3.6",
+    install_requires=["requests==2.32.3", "beautifulsoup4==4.12.3", "pandas==2.2.2"],
+    python_requires = ">=3.13",
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.13',
         'Operating System :: OS Independent'
     ]
 )
